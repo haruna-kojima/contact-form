85c4e6d9f4674f2c3ecc5f8ee46c669cb3b35471
haruna-kojima <rum2254@outlook.jp>
2026-08-15 10:05:44 +0900

    feat: 認証機能の実装（Fortify） #3

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+// 仮ルート（Chapter 6で本実装に置き換え）
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', fn() => 'タスク一覧（準備中）')->name('admins.index');
 });
